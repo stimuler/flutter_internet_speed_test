7dc552e84335a34f1b91f876f1c27a1670b08b88
AkshatBaranwal <akshatbaranwal07@gmail.com>
2024-06-12 00:36:06 +0530

    chore: upgrade connectivity_plus to ^6.0.3

--- lib/src/speed_test_utils.dart
+++ lib/src/speed_test_utils.dart
@@ -3,7 +3,7 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 Future<bool> isInternetAvailable() async {
   final connectivity = Connectivity();
   final connectivityResult = await connectivity.checkConnectivity();
-  return connectivityResult == ConnectivityResult.mobile ||
-      connectivityResult == ConnectivityResult.wifi || 
-      connectivityResult == ConnectivityResult.ethernet;
+  return connectivityResult.contains(ConnectivityResult.mobile) ||
+      connectivityResult.contains(ConnectivityResult.wifi) ||
+      connectivityResult.contains(ConnectivityResult.ethernet);
 }
